c9086757e7f31cea8662edf049d7c710d7aaf5a2
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-26 13:57:13 +0530

    Updating some changes in uc2

--- src/main/java/com/example/userregistration/UserRegistration.java
+++ src/main/java/com/example/userregistration/UserRegistration.java
@@ -7,4 +7,10 @@ public class UserRegistration {
             throw new InvalidUserDetailException("Invalid First Name");
         return true;
     }
+	
+	public boolean validateLastName(String lastName) throws InvalidUserDetailException {
+        if (lastName == null || !lastName.matches("^[A-Z][a-z]{2,}$"))
+            throw new InvalidUserDetailException("Invalid Last Name");
+        return true;
+    }
 }
