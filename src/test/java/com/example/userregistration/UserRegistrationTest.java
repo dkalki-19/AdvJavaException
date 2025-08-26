9a47095e1fce760b7ee7e4cc467b2753c6ffe266
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-26 16:06:10 +0530

    Updating JUnit Test to validate for user details

--- src/test/java/com/example/userregistration/UserRegistrationTest.java
+++ src/test/java/com/example/userregistration/UserRegistrationTest.java
@@ -1,5 +1,19 @@
 package com.example.userregistration;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class UserRegistrationTest {
+	
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
+	    UserRegistration user = new UserRegistration();
+	    assertTrue(user.validateFirstName("John"));
+	}
+
+	@Test
+	public void givenFirstName_WhenShort_ShouldReturnFalse() {
+	    UserRegistration user = new UserRegistration();
+	    assertFalse(user.validateFirstName("Jo"));
+	}
 
 }
