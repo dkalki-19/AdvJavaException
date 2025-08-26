c656a78ea23f774015ab0edecb134d4c67ffd950
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-26 14:20:01 +0530

    basic commit

--- src/main/java/com/example/userregistration/UserRegistration.java
+++ src/main/java/com/example/userregistration/UserRegistration.java
@@ -9,9 +9,4 @@ public class UserRegistration {
         return lastName.matches("^[A-Z][a-z]{2,}$");
     }
 	
-	public boolean validateLastName(String lastName) throws InvalidUserDetailException {
-        if (lastName == null || !lastName.matches("^[A-Z][a-z]{2,}$"))
-            throw new InvalidUserDetailException("Invalid Last Name");
-        return true;
-    }
 }
