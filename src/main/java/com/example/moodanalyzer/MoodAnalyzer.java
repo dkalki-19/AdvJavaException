904fa80d13e8b702d4f8524441f9aa89b63a3dc7
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-08-26 14:07:13 +0530

    UC1: first name validation (+ happy/sad tests)

--- src/main/java/com/example/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/example/moodanalyzer/MoodAnalyzer.java
@@ -2,6 +2,9 @@ package com.example.moodanalyzer;
 
 public class MoodAnalyzer {
 	
-	
+	public String analyseMood(String message) {
+        if (message.contains("Sad")) return "SAD";
+        return "HAPPY";
+    }
 
 }
--- src/main/java/com/example/userregistration/UserRegistration.java
+++ src/main/java/com/example/userregistration/UserRegistration.java
@@ -1,6 +1,7 @@
 package com.example.userregistration;
 
 public class UserRegistration {
-
-	
+    public boolean validateFirstName(String firstName) {
+        return firstName.matches("^[A-Z][a-z]{2,}$");
+    }
 }
